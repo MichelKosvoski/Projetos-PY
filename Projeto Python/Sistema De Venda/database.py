d57e32fd99923e1fc13a81a0ad86bed5cf38303b
MichelKosvosi <sharksoareskosvoski@gmail.com>
2024-07-18 22:20:14 -0400

    Bot de automação cadastro itens

--- Projeto Python/Sistema De Venda/database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("Sistema.db")
-
-cursor= conn.cursor()
-
-cursor.execute("CREATE TABLE IF NOT EXISTS user (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Username TEXT NOT NULL,Email TEXT NOT NULL, Password TEXT NOT NULL, ConfPassword TEXT NOT NULL)")
-
-print("Conexao ao banco de dados feita com sucesso!")
